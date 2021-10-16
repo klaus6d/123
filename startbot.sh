84321df3600de5a99abe5b1529099a557f6ec560
klaus6d <92382046+klaus6d@users.noreply.github.com>
2021-10-16 22:10:23 +0800

    Update startbot.sh

--- startbot.sh
+++ startbot.sh
@@ -4,10 +4,10 @@
 
 
 
-/cd /home/her
+#/cd /home/her
 #npm install --unsafe-perm=true --allow-root
 
 echo "Loading startup.sh..."
-bash /home/startup.sh
+#bash /home/startup.sh
 echo "Starting Bot..."
-nohup java   -jar  /home/q/joal/jack-of-all-trades-2.1.26.jar --joal-conf="/home/q/joal"
+nohup java   -jar  /home/jack-of-all-trades-2.1.26.jar --joal-conf="/home/"
